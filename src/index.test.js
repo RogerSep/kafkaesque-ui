7270a6f1b6b5322e3637b4f55b7a1f44aad62829
Roger Sepulveda <roger.planetgreen@gmail.com>
2016-10-31 18:48:35 -0500

    feat: A test

--- src/index.test.js
+++ src/index.test.js
@@ -1,9 +1,10 @@
 import { expect } from 'chai';
-import defi from './index';
+import defi, { anotherTest } from './index';
 
 describe("kafkesque", () => {
   it("should have tests", () => {
     defi();
+    anotherTest();
     expect(true).to.be.true;
   });
 });
