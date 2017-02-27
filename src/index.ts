fa1b5f0b52d2a52f9d511ce7dc67c2a343a95d92
Roger Sepulveda <roger.planetgreen@gmail.com>
2017-02-27 13:48:18 -0500

    Changed the module definition to a var so it can be used globally

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
-import { topic } from "./topics/topic"
+import { topic as t } from "./topics/topic"
 import { leader } from "./tabs/leader"
 import { Observable } from "rx"
 
 const g = leader(Observable.interval(1000))
-function start(f: () => void = () => {}): void {
+function s(f: () => void = () => {}): void {
   g(f)
 }
 
-export { start }
-export { topic }
+namespace Kui {
+  
+  export function start(f: () => void = () => {}): void {
+    return s(f)
+  }
+
+  export function topic(name: string) {
+    return t(name)
+  }
+
+}
+
+export { s as start }
+export { t as topic }
+export default Kui
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    libraryTarget: "umd",
+    libraryTarget: "var",
     library: "Kui",
     umdNamedDefine: true
   },
